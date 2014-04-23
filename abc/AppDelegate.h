52f9cd161bb6c6b3befca67c0ac8315e8021b93a
Ibokan <ibokan@12-07.local>
2014-04-23 15:21:44 +0800

    Initial Commit

---
+++ abc/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  abc
+//
+//  Created by Ibokan on 14-4-23.
+//  Copyright (c) 2014年 CainiaoLiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
